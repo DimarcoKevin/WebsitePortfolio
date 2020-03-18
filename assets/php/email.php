108dd52a32c7be65a61317775b29282b953abae7
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2020-03-18 19:47:09 -0400

    Adding to, header, subject variables

--- assets/php/email.php
+++ assets/php/email.php
@@ -1,5 +1,15 @@
 <?php
+	// gathering name, email, and message from contact form
 	$name = $_POST['name'];
   	$visitor_email = $_POST['email'];
   	$message = $_POST['message'];
+
+
+  	// adding my email and header
+  	$to = "kevin.j.dimarco@gmail.com";
+  	$header = "From: $visitor_email";
+  	$subject = "Email from kevindimarco.com"
+
+  	mail($to, $subject, $message, $header);
+
 ?>
