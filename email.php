326ce02d86fc640e3e88f5f0433bb1629f733273
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2020-03-19 17:53:22 -0400

    Not using email.php anymore, cleaning up code

--- email.php
+++ email.php
@@ -1,22 +1,19 @@
 <?php
-	if(isset($_POST['submit'])){
-		// gathering name, email, and message from contact form
-		$name = $_POST['name'];
-	  	$visitor_email = $_POST['email'];
-	  	$message = $_POST['message'];
+	// gathering name, email, and message from contact form
+	$name = $_POST['name'];
+  	$visitor_email = $_POST['email'];
+  	$message = $_POST['message'];
 
 
-	  	// adding my email and header
-	  	$to = "kevin.j.dimarco@gmail.com";
-	  	$header = "From: $visitor_email";
-	  	$subject = "Email from kevindimarco.com"
+  	// adding my email and header
+  	$to = "kevin.j.dimarco@gmail.com";
+  	$header = "From: $visitor_email";
+  	$subject = "Email from kevindimarco.com"
 
-	  	// trying to running mail function
-	  	if (mail($to, $subject, $message, $header)) {
-	  		echo "<h4>Thanks for the email, " . $name . "! We will contact you shortly.</h4>";
-	  	} else {
-	  		echo "Something went wrong!";
-	  	}
+  	// trying to running mail function
+  	if (mail($to, $subject, $message, $header)) {
+  		echo "<h4>Thanks for the email, " . $name . "! We will contact you shortly.</h4>";
+  	} else {
+  		echo "Something went wrong!";
   	}
-
 ?>
