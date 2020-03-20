276d892e06cf990a6ae648c955f490fbef73201e
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2020-03-20 15:32:45 -0400

    Changed some variables to clean up code and test

--- email.php
+++ email.php
@@ -1,16 +1,15 @@
 <?php
-
 	if ($_POST) {
 		// gathering name, email, and message from contact form
 		$name = $_POST['name'];
-	  	$visitor_email = $_POST['email'];
+	  	$email = $_POST['email'];
 	  	$message = $_POST['message'];
 
 
 	  	// adding my email and header
 	  	$to = "kevin.j.dimarco@gmail.com";
-	  	$header = "From: $visitor_email";
-	  	$subject = "Email from kevindimarco.com";
+	  	$header = "From: " . $email;
+	  	$subject = "Contact email from portfolio site";
 
 	  	// trying to running mail function
 	  	if (mail($to, $subject, $message, $header)) {
