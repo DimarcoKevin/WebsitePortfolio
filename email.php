716bc55279ec5e3eb1863e56ce916f87c0dba9bf
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2020-03-19 22:51:02 -0400

    Adding POST check

--- email.php
+++ email.php
@@ -1,19 +1,22 @@
 <?php
-	// gathering name, email, and message from contact form
-	$name = $_POST['name'];
-  	$visitor_email = $_POST['email'];
-  	$message = $_POST['message'];
+
+	if ($_POST) {
+		// gathering name, email, and message from contact form
+		$name = $_POST['name'];
+	  	$visitor_email = $_POST['email'];
+	  	$message = $_POST['message'];
 
 
-  	// adding my email and header
-  	$to = "kevin.j.dimarco@gmail.com";
-  	$header = "From: $visitor_email";
-  	$subject = "Email from kevindimarco.com"
+	  	// adding my email and header
+	  	$to = "kevin.j.dimarco@gmail.com";
+	  	$header = "From: $visitor_email";
+	  	$subject = "Email from kevindimarco.com"
 
-  	// trying to running mail function
-  	if (mail($to, $subject, $message, $header)) {
-  		echo "<h4>Thanks for the email, " . $name . "! We will contact you shortly.</h4>";
-  	} else {
-  		echo "Something went wrong!";
-  	}
+	  	// trying to running mail function
+	  	if (mail($to, $subject, $message, $header)) {
+	  		echo "<h4>Thanks for the email, " . $name . "! We will contact you shortly.</h4>";
+	  	} else {
+	  		echo "Something went wrong!";
+	  	}
+	}
 ?>
