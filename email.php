064bc493c15bc203cb04fafd77da72f2b1021b2e
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2020-03-24 17:40:44 -0400

    Undoing alert box since it did not work as intended

--- email.php
+++ email.php
@@ -16,11 +16,7 @@
 	  	if (isset($name, $email, $message)) {
 		  	// trying to running mail function
 		  	if (mail($to, $subject, $message, $header)) {
-			 	print '<script type="text/javascript">'; 
-			    print 'alert("Thanks for the email, " . $name . "! We will contact you shortly.")'; 
-			    print '</script>';
-
-		  		//echo "<h4>Thanks for the email, " . $name . "! We will contact you shortly.</h4>";
+		  		echo "<h4>Thanks for the email, " . $name . "! We will contact you shortly.</h4>";
 		  	} else {
 		  		echo "Something went wrong!";
 		  	}
